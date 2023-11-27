0efedff81e57fd501f817fe0fea32ec5e8ee84af
Mingyu0626 <mg010626@naver.com>
2023-11-27 10:04:04 +0900

    Docs | 전체 레이아웃 및 액티비티 추가

---
+++ app/src/main/java/com/example/sleeper/MyTitleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sleeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyTitleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_title)
+    }
+}
